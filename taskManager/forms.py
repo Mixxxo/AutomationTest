842b9a29e0e35f7651779218aab06fbdb2e63be9
Mixxxo <chunked@hotmail.com>
2024-11-09 21:43:03 +0200

    test branch test PR.

--- taskManager/forms.py
+++ taskManager/forms.py
@@ -12,7 +12,7 @@
 # from django.nV for use in another web application!
 #
 
-""" forms.py contains various Django forms for the applications tester"""
+""" forms.py contains various Django forms for the applications testerr"""
 
 from taskManager.models import Project, Task
 from django import forms
